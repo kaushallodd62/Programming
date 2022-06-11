e8ebb251f223ed9ed3a314460428cc349da9ea69
Kaushal Lodd <kaushallodd62@gmail.com>
2022-06-12 01:43:42 +0530

    commit

--- DSA/Binary Search Trees/avl_insertion.cpp
+++ DSA/Binary Search Trees/avl_insertion.cpp
@@ -2,3 +2,26 @@
 #include "declaration.h"
 using namespace std;
 
+int height(Node* root) {
+    if(root == NULL)
+        return 0;
+    int lh, rh;
+    lh = height(root->left);
+    rh = height(root->right);
+}
+
+Node* insert_node(Node* root, int val) {
+    if(root == NULL) {
+        Node* root = new Node();
+        root->left = root->right = NULL;
+        root->data = val;
+        return root;
+    }
+    if(val < root->data) {
+        root->left = insert_node(root->left, val);
+    }
+    else if(val > root->data) {
+        root->right = insert_node(root->right, val);
+    }
+    return root;   
+}
